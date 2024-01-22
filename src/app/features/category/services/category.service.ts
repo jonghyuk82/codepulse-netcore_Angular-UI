2ec00a5ecfb4c6b249c8e75341402dac92e4d098
John Lee <john.lee@wfs.aero>
2024-01-22 10:14:46 -0500

    End of Section 4

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,15 +2,25 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>(`https://localhost:7101/api/categories`, model);
+    return this.http.post<void>(
+      `${environment.apiBaseUrl}/api/categories`,
+      model
+    );
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(
+      `${environment.apiBaseUrl}/api/Categories`
+    );
   }
 }
